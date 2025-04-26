0f8c608c3d0bcd7d32dc641ee8e1da0a66861cfd
cgentle1618 <cgentle1618@gmail.com>
2025-04-25 17:45:28 -0700

    implementing movement

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
