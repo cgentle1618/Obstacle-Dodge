1ce167fe733dd6eff5b262ec56246ad264d83a6f
cgentle1618 <cgentle1618@gmail.com>
2025-04-28 14:58:28 -0700

    add cinemachine camera, add walls; implement movement codes into function

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 15f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,11 @@ public class Mover : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
